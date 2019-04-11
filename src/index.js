103c3085058044991e8a4f9ad577ad8036c50b3b
Tadeusz Łazurski <tadeusz@lazurski.pl>
2019-04-11 12:46:12 +0200

    Extract hardcoded cities and roles data to config file
    The file is in YML format (for readability). Data is exposed as
    SalaryCalculator.defaults and can be passed to init as optional second argument.
    Alternative config can be provided the same way.
    
    The Elm app doesn't work at this stage.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { Elm } from "./SalaryCalculator.elm"
+import config from "../config.yml"
 
 const program = Elm.SalaryCalculator
 
-export function init(node) {
+export function init(node, config = config) {
   program.init({
-      flags: location.href,
-      node: node
+      flags: {
+        url: location.href,
+        config
+      },
+      node
   })
 }
+
+export const defaults = config
