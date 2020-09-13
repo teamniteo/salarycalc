7aa22fb3e9479f203f2e6fc650720afe21afa541
Nejc Zupan <nejc.zupan@gmail.com>
2020-09-13 10:48:27 +0200

    add support for ESLint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { Elm } from "./Main.elm"
-import config from "../config.yml"
+import { Elm } from "./Main.elm";
+import config from "../config.yml";
 
-const program = Elm.Main
+const program = Elm.Main;
 
 export function init(node, config = config) {
   program.init({
-      flags: {
-        location: location.href,
-        config
-      },
-      node
-  })
+    flags: {
+      location: location.href,
+      config
+    },
+    node
+  });
 }
 
-export const defaults = config
+export const defaults = config;
